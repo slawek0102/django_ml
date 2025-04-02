618283c21bc2e96f61b481ccdbd2b3a2d6c651a6
Sławomir Grzebyk <grzebyk.slawomir@gmail.com>
2025-04-02 21:59:52 +0200

    format navbar

--- projects/views.py
+++ projects/views.py
@@ -5,8 +5,11 @@ from django.http import HttpResponse
 def projects(request):
     return render(request, 'projects.html')
 
+
 def project(request, project_id):
     return render(request, 'single-project.html', {'project': project_id})
+
+
 def home(request):
-    msg ='Home Page w Django ze stylowaniem bootstrap'
+    msg = 'Home Page w Django ze stylowaniem bootstrap'
     return render(request, 'home.html', {'message': msg})
